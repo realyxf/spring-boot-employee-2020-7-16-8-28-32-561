ded2641944c80e23f86336424a08adb7c0a93af0
realyxf <997915421@qq.com>
2020-07-29 11:54:58 +0800

    [Spring Boot Employee] [Fennie] test:should_return_2_companies_when_getallcompany_given_2_companies

--- src/test/java/com/thoughtworks/springbootemployee/CompanyServiceTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/CompanyServiceTest.java
@@ -1,7 +1,40 @@
 package com.thoughtworks.springbootemployee;
 
+import com.thoughtworks.springbootemployee.entity.Company;
+import com.thoughtworks.springbootemployee.repository.CompanyRepository;
+import com.thoughtworks.springbootemployee.service.CompanyService;
+import com.thoughtworks.springbootemployee.service.CompanyServiceImpl;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
 public class CompanyServiceTest {
+    @Mock
+    CompanyRepository companyRepository;
+
+    @InjectMocks
+    private CompanyServiceImpl companyService;
+
+    @Test
+    void should_return_2_companies_when_getallcompany_given_2_companies() {
+
+        // given
+        List<Company> companies = Collections.nCopies(2, new Company());
+        Mockito.when(companyRepository.getAllCompany()).thenReturn(companies);
+        // when
+        List<Company> companiesQueried = companyService.getAllCompany();
+        // then
+        assertEquals(2, companiesQueried.size());
+    }
+
 
 }
