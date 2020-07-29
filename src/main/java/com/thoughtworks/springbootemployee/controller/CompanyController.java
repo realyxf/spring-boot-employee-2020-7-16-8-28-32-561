7357b29703c5ecd1b987a754dc08614bd68786fb
realyxf <997915421@qq.com>
2020-07-29 18:06:16 +0800

    [Spring Boot Employee] [Fennie Jay] feature: complete CompanyController

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -1,50 +1,55 @@
-//package com.thoughtworks.springbootemployee.controller;
-//
-//import com.thoughtworks.springbootemployee.entity.Company;
-//import com.thoughtworks.springbootemployee.entity.Employee;
-//import com.thoughtworks.springbootemployee.service.CompanyServiceImpl;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@RestController
-//public class CompanyController {
-//
-//    @Autowired
-//    CompanyServiceImpl companyServiceImpl;
-//
-//    @PostMapping(path = "/companies")
-//    public void addCompany(@RequestBody Company company) {
-//        companyServiceImpl.addCompany(company);
-//    }
-//
-//    @GetMapping(path = "/companies")
-//    public List<Company> getCompanyOfCurPage(@RequestParam(required = false) Integer page,
-//                                             @RequestParam(required = false) Integer pageSize) {
-//        if (page == null || pageSize == null) {
-//            return companyServiceImpl.getAllCompany();
-//        }
-//        return companyServiceImpl.getCompanyOfCurPage(page, pageSize);
-//    }
-//
-//    @GetMapping(path = "/companies/{companyId}")
-//    public Company getCompany(@PathVariable int companyId) {
-//        return companyServiceImpl.getCompany(companyId);
-//    }
-//
-//    @GetMapping(path = "/companies/{companyId}/employees")
-//    public List<Employee> getEmployeesOfCompany(@PathVariable int companyId) {
-//        return companyServiceImpl.getCompany(companyId).getEmployeeList();
-//    }
-//
-//    @PutMapping(path = "/companies/{companyId}")
-//    public void updateCompany(@PathVariable int companyId, @RequestBody Company newCompany) {
-//        companyServiceImpl.updateCompany(newCompany);
-//    }
-//
-//    @DeleteMapping(path = "/companies/{companyId}")
-//    public void deleteCompany(@PathVariable int companyId) {
-//        companyServiceImpl.deleteCompany(companyId);
-//    }
-//}
+package com.thoughtworks.springbootemployee.controller;
+
+import com.thoughtworks.springbootemployee.entity.Company;
+import com.thoughtworks.springbootemployee.entity.Employee;
+import com.thoughtworks.springbootemployee.service.CompanyServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/companies")
+public class CompanyController {
+
+    @Autowired
+    CompanyServiceImpl companyService;
+
+    @GetMapping
+    public List<Company> getAllCompanies() {
+        return companyService.getAllCompany();
+    }
+
+    @PostMapping
+    public void addCompany(@RequestBody Company company) {
+        companyService.addCompany(company);
+    }
+
+    @GetMapping(params = {"page", "pageSize"})
+    public List<Company> getCompanyOfCurPage(@RequestParam Integer page,
+                                             @RequestParam Integer pageSize) {
+        return companyService.getCompanyOfCurPage(page, pageSize);
+    }
+
+    @GetMapping(path = "/{companyId}")
+    public Company getCompany(@PathVariable int companyId) {
+        return companyService.getCompany(companyId);
+    }
+
+
+    @GetMapping(path = "/{companyId}/employees")
+    public List<Employee> getEmployeesOfCompany(@PathVariable int companyId) {
+        return companyService.getEmployeesOfCompany(companyId);
+    }
+
+
+    @PutMapping(path = "/{companyId}")
+    public void updateCompany(@PathVariable int companyId, @RequestBody Company newCompany) {
+        companyService.updateCompany(newCompany);
+    }
+
+    @DeleteMapping(path = "/{companyId}")
+    public void deleteCompany(@PathVariable int companyId) {
+        companyService.deleteCompany(companyId);
+    }
+}
