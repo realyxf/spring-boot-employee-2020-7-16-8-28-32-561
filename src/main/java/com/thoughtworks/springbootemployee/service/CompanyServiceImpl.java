05b2a7c315d51a79815ad965904e3c41aa61a91f
realyxf <997915421@qq.com>
2020-07-29 18:06:46 +0800

    [Spring Boot Employee] [Fennie Jay] feature: complete CompanyServiceImpl

--- src/main/java/com/thoughtworks/springbootemployee/service/CompanyServiceImpl.java
+++ src/main/java/com/thoughtworks/springbootemployee/service/CompanyServiceImpl.java
@@ -3,6 +3,7 @@ package com.thoughtworks.springbootemployee.service;
 import com.thoughtworks.springbootemployee.entity.Company;
 import com.thoughtworks.springbootemployee.entity.Employee;
 import com.thoughtworks.springbootemployee.repository.CompanyRepository;
+import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,50 +11,54 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class CompanyServiceImpl {
-    //private List<Company> companyList = new ArrayList<>();
+public class CompanyServiceImpl implements CompanyService {
+
+    private final CompanyRepository companyRepository;
 
     @Autowired
-    private CompanyRepository companyRepository;
+    public CompanyServiceImpl(CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
+    }
 
+    @Override
     public List<Company> getAllCompany() {
-        return companyRepository.getAllCompany();
+        return companyRepository.findAll();
     }
-//
-//    public Company getCompany(int companyId) {
-//        return companyList.get(companyId);
-//    }
-//
-//    public List<Employee> getEmployeesOfCompany(int companyId) {
-//        return companyList.get(companyId).getEmployeeList();
-//    }
-//
-//    public List<Company> getCompanyOfCurPage(int page, int pageSize) { //TODO fixbug
-//        List<Company> curPageCompany = new ArrayList<>();
-//        if (page == 1) {
-//            for (int companyItem = 0; companyItem < pageSize; companyItem++) {
-//                curPageCompany.add(companyList.get(companyItem));
-//            }
-//        }
-//        else {
-//            for (int companyItem = (pageSize * (page - 1)); companyItem < page * pageSize; companyItem++) {
-//                curPageCompany.add(companyList.get(companyItem));
-//            }
-//        }
-//        return curPageCompany;
-//    }
-//
-    public void addCompany(Company company) {
-        companyRepository.addCompany(company);
-    }
-//
-//    public void updateCompany(Company newCompany) {
-//        companyList.removeIf(company -> company.getId() == newCompany.getId());
-//        companyList.add(newCompany);
-//    }
-//
-//    public void deleteCompany(int companyId){
-//        companyList.removeIf(company -> company.getId() == companyId);
-//    }
 
+    @Override
+    public Company getCompany(int companyId) {
+        return companyRepository.findById(companyId).orElse(null);
+    }
+
+    @Override
+    public List<Employee> getEmployeesOfCompany(int companyId) {
+        Company company = companyRepository.findById(companyId).orElse(null);
+        return company != null ? company.getEmployeesList() : null;
+    }
+
+    @Override
+    public List<Company> getCompanyOfCurPage(int page, int pageSize) {
+        int length = companyRepository.findAll().size();
+
+        if ((page - 1) * pageSize > length - 1) {
+            return new ArrayList<>();
+        }
+        return companyRepository.findAll()
+                .subList(Math.min((page - 1) * pageSize, length - 1), Math.min(length, page * pageSize));
+    }
+
+    @Override
+    public void addCompany(Company company) {
+        companyRepository.save(company);
+    }
+
+    @Override
+    public void updateCompany(Company newCompany) {
+        companyRepository.save(newCompany);
+    }
+
+    @Override
+    public void deleteCompany(int companyId) {
+        companyRepository.deleteById(companyId);
+    }
 }
