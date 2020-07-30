56f3c888bf1767a5091fbdfabb5a23b6f800e9c5
realyxf <997915421@qq.com>
2020-07-30 14:46:05 +0800

    [Spring Boot Employee] [Fennie Karol] feature: complete  should_return_1_company_when_add_1_company_given_1_company

--- src/test/java/com/thoughtworks/springbootemployee/integrationtest/CompanyIntegrationTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/integrationtest/CompanyIntegrationTest.java
@@ -1,15 +1,21 @@
 package com.thoughtworks.springbootemployee.integrationtest;
 
+import com.thoughtworks.springbootemployee.entity.Company;
 import com.thoughtworks.springbootemployee.repository.CompanyRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -33,6 +39,20 @@ public class CompanyIntegrationTest {
 
         mockMvc.perform(MockMvcRequestBuilders.get("/companies")).andExpect(status().isOk());
 
+    }
+
+    @Test
+    void should_return_1_company_when_add_1_company_given_1_company() throws Exception {
+        String companyJson = "{\n" +
+                "    \"companyName\": \"oocl\"\n" +
+                "  }";
+
+        mockMvc.perform(post("/companies").contentType(MediaType.APPLICATION_JSON).content(companyJson)).andExpect(status().isCreated());
+
+        List<Company> companyList = companyRepository.findAll();
+        assertEquals(1,companyList.size());
+
+        assertEquals("oocl",companyList.get(0).getCompanyName());
     }
 
 }
--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -10,20 +10,8 @@ CREATE TABLE employee
     company_id INT
 );
 
-INSERT INTO employee(name, age, gender, company_id) VALUES
-('AAAA','11','male','1'),
-('BBBB','11','male','1'),
-('CCCC','11','female','1'),
-('DDDD','11','male','1'),
-('EEEE','11','female','2'),
-('FFFF','11','female','2'),
-('GGGG','11','female','2'),
-('HHHH','11','female','2');
-
 CREATE TABLE company
 (
     company_id INT AUTO_INCREMENT PRIMARY KEY,
     name varchar(100)
 );
-
-INSERT INTO company(name) VALUES ('oocl') , VALUES ('tw');
